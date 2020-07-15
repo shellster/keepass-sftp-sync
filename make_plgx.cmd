fc3e185d0461a815e6a672af4ebb22e71c055d3a
shellster <zn8v6c3f5nw9@blurmail.net>
2020-07-14 20:25:38 -0600

    Pagent Support + SSH.net update

--- make_plgx.cmd
+++ make_plgx.cmd
@@ -1,3 +1,2 @@
-@echo off
-echo "%CD%\..\bins\KeePass\KeePass.exe" --plgx-create %cd%\SftpSync --plgx-prereq-kp:2.18 --plgx-prereq-net:4.0 --debug
-call "%CD%\..\bins\KeePass\KeePass.exe" --plgx-create %cd%\SftpSync --plgx-prereq-kp:2.18 --plgx-prereq-net:4.0 --debug
+@echo on
+call "%CD%\..\bins\KeePass\KeePass.exe" --plgx-create %cd%\SftpSync --plgx-prereq-kp:2.18 --plgx-prereq-net:4.0
